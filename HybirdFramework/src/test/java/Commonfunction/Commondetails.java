8a0906711964f81c6a8e1c01a7f7261c7c925acd
Lingeshwar <Lingeshwar@LAPTOP-TCIABFM5>
2024-07-04 11:07:18 +0530

    browser

--- HybirdFramework/src/test/java/Commonfunction/Commondetails.java
+++ HybirdFramework/src/test/java/Commonfunction/Commondetails.java
@@ -54,7 +54,7 @@ public class Commondetails {
 		loadbrowser();
 
 		String url = properties.getProperty("URL");
-		String browser = properties.getProperty("browser");
+		String browser = properties.getProperty("Browser");
 		if (browser.equalsIgnoreCase("Chrome")) {
 			WebDriverManager.chromedriver().setup();
 			// System.setProperty("webdriver.driver.chrome",
