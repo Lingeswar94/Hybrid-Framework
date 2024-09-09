2d04cc2659d92d35090ed7a3a05ccd7f74725c8e
Lingeshwar <Lingeshwar@LAPTOP-TCIABFM5>
2024-09-09 16:32:35 +0530

    Adding Parameter in Month and date

--- HybirdFramework/src/test/java/Testcase/SearchTC.java
+++ HybirdFramework/src/test/java/Testcase/SearchTC.java
@@ -34,7 +34,8 @@ public class SearchTC extends Commondetails {
 		page.Tripselection(exceldata.getstringdata("Sheet1", 0, 1));
 		page.departureroute(exceldata.getstringdata("Sheet1", 1, 1));
 		page.arrivalroute(exceldata.getstringdata("Sheet1", 2, 1));
-		page.DateSelection(exceldata.getstringdata("Sheet1", 3, 1), exceldata.getstringdata("Sheet1", 4, 1));
+	//	page.DateSelection(exceldata.getstringdata("Sheet1", 3, 1), exceldata.getstringdata("Sheet1", 4, 1));
+		page.DateSelection(properties.getProperty("MonthandYear"), exceldata.getstringdata("Sheet1", 4, 1));
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		page.ReturnDateselection(exceldata.getstringdata("Sheet1", 5, 1), exceldata.getstringdata("Sheet1", 6, 1));
 		page.Adultcount(exceldata.getstringdata("Sheet1", 7, 1));
